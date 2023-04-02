6d91b734a378fdedb798ea6ba0c3ece0d2323634
Per Lenander <per.lenander@addiva.se>
2023-04-02 10:38:42 +0200

    Player score block dynamically instantiated

--- Assets/UI/UIPrefab.cs
+++ Assets/UI/UIPrefab.cs
@@ -9,6 +9,9 @@ public class UIPrefab : MonoBehaviour
     public GameObject UIContainer;
     public GameObject playerScorePrefab;
 
+    const int playerHeight = 30;
+
+    int numPlayers = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -24,11 +27,20 @@ public class UIPrefab : MonoBehaviour
 
     public IEnumerator OnPlayerJoined(PlayerInput playerInput)
     {
+        // TODO: Offset is not entirely correct, unclear why
+        numPlayers++;
+        // Resize the UI container panel to make space for the new player
+        RectTransform tf = UIContainer.GetComponent<RectTransform>();
+        tf.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, numPlayers * playerHeight);
+
+        // Create a new scoreboard element and add it to the UI container
         Player player = playerInput.gameObject.GetComponent<Player>();
-        GameObject scoreUI = GameObject.Instantiate<GameObject>(playerScorePrefab, UIContainer.transform);
+        Transform UITransform = UIContainer.transform;
+        GameObject scoreUI = GameObject.Instantiate<GameObject>(playerScorePrefab, UITransform);
+        //scoreUI.transform.position += new Vector3(0, (numPlayers - 1) * playerHeight, 0);
         Image image = scoreUI.GetComponentInChildren<Image>();
         Material avatarMat = image.material;
-        // TODO: Player color is not set in Start yet
+        // Player color is not set in Start yet, we have to wait until it is
         yield return new WaitUntil(() => player.IsInitialized);
         avatarMat.SetColor("_Color", player.color);
     }
